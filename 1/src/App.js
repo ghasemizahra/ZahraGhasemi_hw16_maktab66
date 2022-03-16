8a516e0b0ed9926171978107e5502026d65db8c5
ghasemizahra <z.ghasemi.02018@gmail.com>
2022-03-16 23:29:43 +0330

    add

--- 1/src/App.js
+++ 1/src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState, useRef, useCallback } from "react";
+import Button from "./Button";
+import Title from "./Title";
 
 function App() {
+  console.log("App is rendering...");
+  useEffect(() => console.log("== App rendered =="));
+
+  let [value1, setValue1] = useState()
+  let [value2, setValue2] = useState()
+
+  let ref1 = useRef()
+  let ref2 = useRef()
+
+  const changeValue1 = useCallback(() => {
+    setValue1(ref1.current.value)
+    ref1.current.value = ''
+  }, [value1]);
+
+  const changeValue2 = useCallback(() => {
+    setValue2(ref2.current.value)
+    ref2.current.value = ''
+  }, [value2]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="value-1">
+        first value: <Title>{value1}</Title>
+        <br />
+        change first value:
+        <input ref={ref1} />
+        <Button onClick={changeValue1}> change </Button>
+      </div>
+      <div className="value-2">
+        second value: <Title>{value2}</Title>
+        <br />
+        change second value:
+        <input ref={ref2}/>
+        <Button onClick={changeValue2}> change </Button>
+      </div>
     </div>
   );
 }
